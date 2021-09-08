de3a6336dd6df2dbc5b9c194b594ad2a6621aa19
EmmanuelLeguizamon <ema.legui.carp@gmail.com>
2021-09-08 20:15:33 -0300

    Ejercicio 18

--- EjercicioXVIII/Program.cs
+++ EjercicioXVIII/Program.cs
@@ -6,7 +6,34 @@ namespace EjercicioXVIII
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool isString;
+
+
+            do
+            {
+
+                Console.WriteLine("Ingrese un número (de ser decimal de 5 cifras, mejor): ");
+
+                string n = Console.ReadLine();
+
+                isString = Decimal.TryParse(n, out decimal m);
+
+                if (isString == false)
+                {
+                    Console.WriteLine("No se ingresó un número válido");
+
+                }else
+                {
+                    m = decimal.Round(m, 2);
+                    Console.WriteLine("Su número redondeado a 2 decimales es: " + m);
+                }
+
+            } while (isString == false);
+
+            Console.ReadKey();
+            Console.Clear();
+
+
         }
     }
 }
